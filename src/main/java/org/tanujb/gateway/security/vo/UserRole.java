b5d5689909f2cb45bd6a61d4f6a2c5a293a15596
Tanuj Bhakre <tanujbhakre89@gmail.com>
2016-03-18 19:05:00 +0530

    Updating roles to be prefixed with Roles

--- src/main/java/org/tanujb/gateway/security/vo/UserRole.java
+++ src/main/java/org/tanujb/gateway/security/vo/UserRole.java
@@ -2,16 +2,17 @@ package org.tanujb.gateway.security.vo;
 
 /**
  * Different possible roles a user can have
+ * 
  * @author tbhakre
  *
  */
 public enum UserRole {
-	USER("USER"), ADMIN("ADMIN");
-	
+	USER("ROLE_USER"), ADMIN("ROLE_ADMIN");
+
 	private String roleName;
-	
-	UserRole(String roleName){
-		this.roleName=roleName;
+
+	UserRole(String roleName) {
+		this.roleName = roleName;
 	}
 
 	public String getRoleName() {
