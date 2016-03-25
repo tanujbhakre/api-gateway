d962bdef3d3223cb0a4450dbf316d825be2d4663
Tanuj Bhakre <tanujbhakre89@gmail.com>
2016-03-25 18:02:59 +0530

    Added Gateway Exception handler. This closes #8

---
+++ src/main/java/org/tanujb/gateway/handler/GatewayExceptionHandler.java
@@ -0,0 +1,51 @@
+package org.tanujb.gateway.handler;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
+import org.tanujb.gateway.exception.ApplicationRuntimeException;
+
+/**
+ * Controller advice for handling exceptions
+ * 
+ * @author tbhakre
+ *
+ */
+@ControllerAdvice
+public class GatewayExceptionHandler {
+
+	/**
+	 * Capturing rest template exceptions and sending response as is.
+	 * 
+	 * @param response
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(HttpClientErrorException.class)
+	@ResponseBody
+	public String httpExceptionHandler(HttpServletResponse response,
+			HttpClientErrorException ex) {
+		response.setStatus(ex.getStatusCode().value());
+		return ex.getResponseBodyAsString();
+	}
+
+	/**
+	 * Capturing runtime exception and responding with error
+	 * 
+	 * @param response
+	 * @param ex
+	 * @return
+	 */
+	@ExceptionHandler(ApplicationRuntimeException.class)
+	@ResponseBody
+	public String applicationExceptionHandler(HttpServletResponse response,
+			ApplicationRuntimeException ex) {
+
+		response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+		return ex.getMessage();
+	}
+}
